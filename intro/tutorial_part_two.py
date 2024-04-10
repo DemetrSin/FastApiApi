1f50e1fd4c4eaff54991c0c3bb32282ce748bda3
Demetr Sin <sonofgod1551@gmail.com>
2024-04-10 10:26:52 +0300

    Processed Create and Connect Rows JOIN tutorials part

--- intro/tutorial_part_two.py
+++ intro/tutorial_part_two.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, SQLModel, create_engine, Session
 from tutorial import Hero, engine, create_db_and_tables
 
 
@@ -8,12 +8,41 @@ class Team(SQLModel, table=True):
     headquarters: str
 
 
-# class HeroWithTeam(Hero):
-#     team_id: int | None = Field(default=None, foreign_key='team.id')
+def create_data():
+    with Session(engine) as session:
+        team_preventers = Team(name="Preventers", headquarters="Sharp Tower")
+        team_z_force = Team(name="Z-Force", headquarters="Sister Margaret's Bar")
+        session.add(team_preventers)
+        session.add(team_z_force)
+        session.commit()
+
+        hero_deadpond = Hero(
+            name="Deadpond", secret_name="Dive Wilson", team_id=team_z_force.id
+        )
+        hero_rusty_man = Hero(
+            name="Rusty-Man",
+            secret_name="Tommy Sharp",
+            age=48,
+            team_id=team_preventers.id,
+        )
+        hero_spider_boy = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+        session.add(hero_deadpond)
+        session.add(hero_rusty_man)
+        session.add(hero_spider_boy)
+        session.commit()
+
+        session.refresh(hero_deadpond)
+        session.refresh(hero_rusty_man)
+        session.refresh(hero_spider_boy)
+
+        print("Created hero:", hero_deadpond)
+        print("Created hero:", hero_rusty_man)
+        print("Created hero:", hero_spider_boy)
 
 
 def main():
     create_db_and_tables()
+    create_data()
 
 
 if __name__ == "__main__":
