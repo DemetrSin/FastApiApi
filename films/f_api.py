a5de007284fb129b383007b7a1eabc17bca4c6e2
Demetr Sin <sonofgod1551@gmail.com>
2024-04-16 09:54:26 +0300

    keep working

--- films/f_api.py
+++ films/f_api.py
@@ -24,6 +24,10 @@
 #     ActorPublicWithFilms,
 #     GenrePublicWithFilms,
 # )
+import sys
+
+import uvicorn
+
 from f_two import (
     FilmProducerAssociation,
     FilmBase,
@@ -32,11 +36,16 @@ from f_two import (
     FilmPublic,
     Producer,
     ProducerCreate,
-    ProducerPublic
+    ProducerPublic,
+    ProducerPublicWithFilms,
+    Actor,
+    ActorCreate,
+    ActorPublic,
+    ActorPublicWithFilms
 )
 from fastapi import FastAPI, Depends
 from database import create_db_and_tables, engine
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 app = FastAPI()
 
@@ -56,13 +65,43 @@ def post_film(
         *,
         session: Session = Depends(get_session),
         film: FilmCreate,
-        producers: list[ProducerCreate]
+        producers: list[ProducerCreate],
+        actors: list[ActorCreate]
 ):
     db_film = Film.model_validate(film)
-    for producer in producers:
-        db_producer = Producer.model_validate(producer)
-        db_film.producers.append(db_producer)
+    # for producer in producers:
+    #     db_producer = session.exec(select(Producer).where(Producer.name == producer.name)).first()
+    #     if db_producer:
+    #         db_producer.films.append(db_film)
+    #     else:
+    #         db_producer = Producer.model_validate(producer)
+    #         db_film.producers.append(db_producer)
+    creation(lst=producers, cls=Producer, session=session, db_film=db_film)
+    creation(lst=actors, cls=Actor, session=session, db_film=db_film)
     session.add(db_film)
     session.commit()
     session.refresh(db_film)
     return db_film
+
+
+def creation(lst, cls, session, db_film):
+    for entity in lst:
+        db_entity = session.exec(select(cls).where(cls.name == entity.name)).first()
+        if db_entity:
+            db_entity.films.append(db_film)
+        else:
+            db_entity = cls.model_validate(entity)
+            db_film.producers.append(db_entity)
+
+
+@app.get('/producers/{producer_id}', response_model=ProducerPublicWithFilms)
+def get_producer(*, session: Session = Depends(get_session), producer_id):
+    db_producer = session.get(Producer, producer_id)
+    return db_producer
+
+
+if __name__ == '__main__':
+    try:
+        uvicorn.run(app)
+    except KeyboardInterrupt:
+        print('Process is done')
--- films/f_two.py
+++ films/f_two.py
@@ -6,6 +6,16 @@ class FilmProducerAssociation(SQLModel, table=True):
     producer_id: int | None = Field(default=None, primary_key=True, foreign_key='producer.id')
 
 
+class FilmActorAssociation(SQLModel, table=True):
+    film_id: int | None = Field(default=True, primary_key=True, foreign_key='film.id')
+    actor_id: int | None = Field(default=True, primary_key=True, foreign_key='actor.id')
+
+
+# class FilmGenreAssociation(SQLModel, table=True):
+#     film_id: int | None = Field(default=None, primary_key=True, foreign_key='film.id')
+#     genre_id: int | None = Field(default=None, primary_key=True, foreign_key='genre.id')
+
+
 class FilmBase(SQLModel):
     name: str = Field(index=True)
     release_date: int
@@ -17,6 +27,7 @@ class FilmBase(SQLModel):
 class Film(FilmBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     producers: list['Producer'] = Relationship(back_populates='films', link_model=FilmProducerAssociation)
+    actors: list['Actor'] = Relationship(back_populates='films', link_model=FilmActorAssociation)
 
 
 class FilmCreate(FilmBase):
@@ -26,6 +37,7 @@ class FilmCreate(FilmBase):
 class FilmPublic(FilmBase):
     id: int
     producers: list['ProducerPublic']
+    actors: list['ActorPublic']
 
 
 class ProducerBase(SQLModel):
@@ -43,4 +55,32 @@ class ProducerCreate(ProducerBase):
 
 class ProducerPublic(ProducerBase):
     id: int
+
+
+class ProducerPublicWithFilms(ProducerBase):
+    id: int
     films: list[Film]
+
+
+class ActorBase(SQLModel):
+    name: str = Field(index=True)
+
+
+class Actor(ActorBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    films: list[Film] = Relationship(back_populates='actors', link_model=FilmActorAssociation)
+
+
+class ActorCreate(ActorBase):
+    pass
+
+
+class ActorPublic(ActorBase):
+    id: int
+
+
+class ActorPublicWithFilms(ActorBase):
+    id: int
+    films: list[FilmPublic]
+
+
