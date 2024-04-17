e9289cf6ebe4a06114739a9af140a8a738156572
Demetr Sin <sonofgod1551@gmail.com>
2024-04-17 10:54:40 +0300

    created class Handler and adapted API to it

--- films/api_routine_handler.py
+++ films/api_routine_handler.py
@@ -3,75 +3,72 @@ from sqlmodel import select
 from films_db_models import Producer, Actor, Genre
 
 
-def creation_routine_handler(lst, cls, session, db_film):
-    """
-       Routine handler for creating related entities and associating them with a film.
+class ApiRoutineHandler:
+    @staticmethod
+    def creation_routine_handler(lst, cls, session, db_film):
+        """
+           Routine handler for creating related entities and associating them with a film.
 
-       This function iterates over a list of entity objects (e.g., producers or actors),
-       checks if each entity already exists in the database, and associates it with the
-       provided film. If the entity does not exist, it is created and associated with the film.
+           This function iterates over a list of entity objects (e.g., producers or actors),
+           checks if each entity already exists in the database, and associates it with the
+           provided film. If the entity does not exist, it is created and associated with the film.
 
-       Parameters:
-       - lst (list): A list of entity objects to be created or associated with the film.
-       - cls: The class type of the entity (e.g., Producer or Actor).
-       - session (Session): The SQLModel session object for interacting with the database.
-       - db_film: The film object with which the entities will be associated.
+           Parameters:
+           - lst (list): A list of entity objects to be created or associated with the film.
+           - cls: The class type of the entity (e.g., Producer or Actor).
+           - session (Session): The SQLModel session object for interacting with the database.
+           - db_film: The film object with which the entities will be associated.
 
-       Returns:
-       - None
+           Returns:
+           - None
 
-       Example Usage:
-       creation_routine_handler(
-           lst=producers,
-           cls=Producer,
-           session=session,
-           db_film=db_film
-       )
-       """
-    for entity in lst:
-        db_entity = session.exec(select(cls).where(cls.name == entity.name)).first()
-        if db_entity:
-            db_entity.films.append(db_film)
-        else:
-            db_entity = cls.model_validate(entity)
-            if isinstance(db_entity, Producer):
-                db_film.producers.append(db_entity)
-            elif isinstance(db_entity, Actor):
-                db_film.actors.append(db_entity)
-            elif isinstance(db_entity, Genre):
-                db_film.genres.append(db_entity)
+           Example Usage:
+           creation_routine_handler(
+               lst=producers,
+               cls=Producer,
+               session=session,
+               db_film=db_film
+           )
+           """
+        for entity in lst:
+            db_entity = session.exec(select(cls).where(cls.name == entity.name)).first()
+            if db_entity:
+                db_entity.films.append(db_film)
+            else:
+                db_entity = cls.model_validate(entity)
+                if isinstance(db_entity, Producer):
+                    db_film.producers.append(db_entity)
+                elif isinstance(db_entity, Actor):
+                    db_film.actors.append(db_entity)
+                elif isinstance(db_entity, Genre):
+                    db_film.genres.append(db_entity)
 
+    @staticmethod
+    def if_not(*, obj, cls):
+        if not obj:
+            detail = f"{cls.__name__} not Found"
+            raise HTTPException(status_code=404, detail=detail)
 
-def if_not_routine_handler(obj, status_code, detail):
-    if not obj:
-        raise HTTPException(status_code=status_code, detail=detail)
-    return obj
+    def get_object_handler(self, *, cls, obj_id, session):
+        db_obj = session.get(cls, obj_id)
+        self.if_not(obj=db_obj, cls=cls)
+        return db_obj
 
+    def get_objects_handler(self, *, cls, session, offset, limit):
+        db_objects = session.exec(select(cls).offset(offset).limit(limit)).all()
+        self.if_not(obj=db_objects, cls=cls)
+        return db_objects
 
-def if_not(*, obj, cls):
-    if not obj:
-        detail = f"{cls.__name__} not Found"
-        raise HTTPException(status_code=404, detail=detail)
+    @staticmethod
+    def session_routine_handler(obj, session):
+        session.add(obj)
+        session.commit()
+        session.refresh(obj)
+        return obj
 
-
-def get_object_handler(*, cls, obj_id, session):
-    db_obj = session.get(cls, obj_id)
-    if_not(obj=db_obj, cls=cls)
-    return db_obj
-
-
-def session_routine_handler(obj, session):
-    session.add(obj)
-    session.commit()
-    session.refresh(obj)
-    return obj
-
-
-def deletion_handler(*, cls, obj_id, session,):
-    db_obj = session.get(cls, obj_id)
-    if not db_obj:
-        detail = f"{cls.__name__} not Found"
-        raise HTTPException(status_code=404, detail=detail)
-    session.delete(db_obj)
-    session.commit()
-    return {'ok': 'Successful deletion'}
+    def deletion_handler(self, *, cls, obj_id, session,):
+        db_obj = session.get(cls, obj_id)
+        self.if_not(obj=db_obj, cls=cls)
+        session.delete(db_obj)
+        session.commit()
+        return {'ok': 'Successful deletion'}
--- films/films_api.py
+++ films/films_api.py
@@ -25,10 +25,13 @@ from films_db_models import (
 from fastapi import FastAPI, Depends, Query, HTTPException
 from database import create_db_and_tables, engine
 from sqlmodel import Session, select
-from api_routine_handler import creation_routine_handler, if_not_routine_handler, session_routine_handler, deletion_handler, get_object_handler
+# from api_routine_handler import creation_routine_handler, session_routine_handler, deletion_handler, get_object_handler, get_objects_handler
+from api_routine_handler import ApiRoutineHandler
 from sqlalchemy.exc import IntegrityError
 
+
 app = FastAPI()
+handler = ApiRoutineHandler()
 
 
 def get_session():
@@ -55,32 +58,32 @@ def create_film(
 ):
     try:
         db_film = Film.model_validate(film)
-        creation_routine_handler(
+        handler.creation_routine_handler(
             lst=producers,
             cls=Producer,
             session=session,
             db_film=db_film
         )
-        creation_routine_handler(
+        handler.creation_routine_handler(
             lst=actors,
             cls=Actor,
             session=session,
             db_film=db_film
         )
-        creation_routine_handler(
+        handler.creation_routine_handler(
             lst=genres,
             cls=Genre,
             session=session,
             db_film=db_film
         )
-        return session_routine_handler(obj=db_film, session=session)
+        return handler.session_routine_handler(obj=db_film, session=session)
     except IntegrityError:
         raise HTTPException(status_code=400, detail="Film name must be unique")
 
 
 @app.get('/films/{film_id}', response_model=FilmPublicFull)
 def get_film(film_id, session: Session = Depends(get_session)):
-    return get_object_handler(cls=Film, obj_id=film_id, session=session)
+    return handler.get_object_handler(cls=Film, obj_id=film_id, session=session)
 
 
 @app.get('/films/', response_model=list[FilmPublic])
@@ -90,8 +93,7 @@ def get_films(
         offset: int = 0,
         limit: int = Query(default=10, le=10)
 ):
-    db_films = session.exec(select(Film).offset(offset).limit(limit)).all()
-    return if_not_routine_handler(obj=db_films, status_code=404, detail='Films not Found')
+    return handler.get_objects_handler(cls=Film, session=session, offset=offset, limit=limit)
 
 
 @app.patch('/films/{film_id}/', response_model=FilmPublicFull)
@@ -109,12 +111,12 @@ def update_film(film_id: int, film: FilmUpdate, session: Session = Depends(get_s
             db_film.genres = [Genre(**g) for g in value]
         else:
             setattr(db_film, key, value)
-    return session_routine_handler(obj=db_film, session=session)
+    return handler.session_routine_handler(obj=db_film, session=session)
 
 
 @app.delete('/films/{film_id}')
 def delete_films(film_id: int, session: Session = Depends(get_session)):
-    return deletion_handler(cls=Film, obj_id=film_id, session=session)
+    return handler.deletion_handler(cls=Film, obj_id=film_id, session=session)
 
 
 # PRODUCERS PART
@@ -123,12 +125,12 @@ def delete_films(film_id: int, session: Session = Depends(get_session)):
 @app.post('/producers/', response_model=ProducerPublic)
 def create_producer(producer: ProducerCreate, session: Session = Depends(get_session)):
     db_producer = Producer.model_validate(producer)
-    return session_routine_handler(obj=db_producer, session=session)
+    return handler.session_routine_handler(obj=db_producer, session=session)
 
 
 @app.get('/producers/{producer_id}', response_model=ProducerPublicWithFilms)
 def get_producer(*, session: Session = Depends(get_session), producer_id):
-    return get_object_handler(cls=Producer, obj_id=producer_id, session=session)
+    return handler.get_object_handler(cls=Producer, obj_id=producer_id, session=session)
 
 
 @app.get('/producers/', response_model=list[ProducerPublic])
@@ -137,8 +139,7 @@ def get_producers(
         offset: int = 0,
         limit: int = Query(default=10, le=10)
 ):
-    db_producers = session.exec(select(Producer).offset(offset).limit(limit)).all()
-    return if_not_routine_handler(obj=db_producers, status_code=404, detail='Producers not Found')
+    return handler.get_objects_handler(cls=Producer, session=session, offset=offset, limit=limit)
 
 
 @app.patch('/producers/{producer_id}', response_model=ProducerPublicWithFilms)
@@ -153,12 +154,12 @@ def update_producer(
     producer_data = producer.model_dump(exclude_unset=True)
     for key, value in producer_data.items():
         setattr(db_producer, key, value)
-    return session_routine_handler(obj=db_producer, session=session)
+    return handler.session_routine_handler(obj=db_producer, session=session)
 
 
 @app.delete('/producers/{producer_id}')
 def delete_producer(producer_id: int, session: Session = Depends(get_session)):
-    return deletion_handler(cls=Producer, obj_id=producer_id, session=session)
+    return handler.deletion_handler(cls=Producer, obj_id=producer_id, session=session)
 
 
 # ACTORS PART
@@ -167,12 +168,12 @@ def delete_producer(producer_id: int, session: Session = Depends(get_session)):
 @app.post('/actors/', response_model=ActorPublic)
 def create_actor(actor: ActorCreate, session: Session = Depends(get_session)):
     db_actor = Actor.model_validate(actor)
-    return session_routine_handler(obj=db_actor, session=session)
+    return handler.session_routine_handler(obj=db_actor, session=session)
 
 
 @app.get('/actors/{actor_id}', response_model=ActorPublicWithFilms)
 def get_actor(*, session: Session = Depends(get_session), actor_id):
-    return get_object_handler(cls=Actor, obj_id=actor_id, session=session)
+    return handler.get_object_handler(cls=Actor, obj_id=actor_id, session=session)
 
 
 @app.get('/actors/', response_model=list[ActorPublic])
@@ -181,8 +182,7 @@ def get_actors(
         offset: int = 0,
         limit: int = Query(default=10, le=10)
 ):
-    db_actors = session.exec(select(Actor).offset(offset).limit(limit)).all()
-    return if_not_routine_handler(obj=db_actors, status_code=404, detail='Actors not Found')
+    return handler.get_objects_handler(cls=Actor, session=session, offset=offset, limit=limit)
 
 
 @app.patch('/actors/{actor_id}', response_model=ActorPublicWithFilms)
@@ -197,12 +197,12 @@ def update_actor(
     actor_data = actor.model_dump(exclude_unset=True)
     for key, value in actor_data.items():
         setattr(db_actor, key, value)
-    return session_routine_handler(obj=db_actor, session=session)
+    return handler.session_routine_handler(obj=db_actor, session=session)
 
 
 @app.delete('/actors/{actor_id}')
 def delete_actor(actor_id: int, session: Session = Depends(get_session)):
-    return deletion_handler(cls=Actor, obj_id=actor_id, session=session)
+    return handler.deletion_handler(cls=Actor, obj_id=actor_id, session=session)
 
 
 # GENRES PART
@@ -211,12 +211,12 @@ def delete_actor(actor_id: int, session: Session = Depends(get_session)):
 @app.post('/genres/', response_model=GenrePublic)
 def create_genre(genre: GenreCreate, session: Session = Depends(get_session)):
     db_genre = Genre.model_validate(genre)
-    return session_routine_handler(obj=db_genre, session=session)
+    return handler.session_routine_handler(obj=db_genre, session=session)
 
 
 @app.get('/genres/{genre_id}/', response_model=GenrePublicWithFilms)
 def get_genre(genre_id, session: Session = Depends(get_session)):
-    return get_object_handler(cls=Genre, obj_id=genre_id, session=session)
+    return handler.get_object_handler(cls=Genre, obj_id=genre_id, session=session)
 
 
 @app.get('/genres/', response_model=list[GenrePublic])
@@ -225,8 +225,7 @@ def get_genres(
         offset: int = 0,
         limit: int = Query(default=10, le=10)
 ):
-    db_genres = session.exec(select(Genre).offset(offset).limit(limit)).all()
-    return if_not_routine_handler(obj=db_genres, status_code=404, detail='Genres not Found')
+    return handler.get_objects_handler(cls=Genre, session=session, offset=offset, limit=limit)
 
 
 @app.patch('/genres/{genre_id}', response_model=GenrePublicWithFilms)
@@ -241,12 +240,12 @@ def update_genre(
     genre_data = genre.model_dump(exclude_unset=True)
     for key, value in genre_data.items():
         setattr(db_genre, key, value)
-    return session_routine_handler(obj=db_genre, session=session)
+    return handler.session_routine_handler(obj=db_genre, session=session)
 
 
 @app.delete('/genres/{genre_id}')
 def delete_genre(genre_id: int, session: Session = Depends(get_session)):
-    return deletion_handler(cls=Genre, obj_id=genre_id, session=session)
+    return handler.deletion_handler(cls=Genre, obj_id=genre_id, session=session)
 
 
 if __name__ == '__main__':
