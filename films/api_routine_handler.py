183c755a780a50f1a6fa6765a27df23cbf5d4727
Demetr Sin <sonofgod1551@gmail.com>
2024-04-17 07:33:27 +0300

    added handlers for routine funcs

---
+++ films/api_routine_handler.py
@@ -0,0 +1,53 @@
+from fastapi import HTTPException
+from sqlmodel import select
+from films_db_models import Producer, Actor
+
+
+def creation_routine_handler(lst, cls, session, db_obj):
+    """
+       Routine handler for creating related entities and associating them with a film.
+
+       This function iterates over a list of entity objects (e.g., producers or actors),
+       checks if each entity already exists in the database, and associates it with the
+       provided film. If the entity does not exist, it is created and associated with the film.
+
+       Parameters:
+       - lst (list): A list of entity objects to be created or associated with the film.
+       - cls: The class type of the entity (e.g., Producer or Actor).
+       - session (Session): The SQLModel session object for interacting with the database.
+       - db_obj: The film object with which the entities will be associated.
+
+       Returns:
+       - None
+
+       Example Usage:
+       creation_routine_handler(
+           lst=producers,
+           cls=Producer,
+           session=session,
+           db_film=db_film
+       )
+       """
+    for entity in lst:
+        db_entity = session.exec(select(cls).where(cls.name == entity.name)).first()
+        if db_entity:
+            db_entity.films.append(db_obj)
+        else:
+            db_entity = cls.model_validate(entity)
+            if isinstance(db_entity, Producer):
+                db_obj.producers.append(db_entity)
+            elif isinstance(db_entity, Actor):
+                db_obj.actors.append(db_entity)
+
+
+def if_not_routine_handler(obj, status_code, detail):
+    if not obj:
+        raise HTTPException(status_code=status_code, detail=detail)
+    return obj
+
+
+def session_routine_handler(obj, session):
+    session.add(obj)
+    session.commit()
+    session.refresh(obj)
+    return obj
--- films/films_api.py
+++ films/films_api.py
@@ -14,9 +14,10 @@ from films_db_models import (
     ActorPublic,
     ActorPublicWithFilms
 )
-from fastapi import FastAPI, Depends, Query, HTTPException
+from fastapi import FastAPI, Depends, Query
 from database import create_db_and_tables, engine
 from sqlmodel import Session, select
+from api_routine_handler import creation_routine_handler, if_not_routine_handler, session_routine_handler
 
 app = FastAPI()
 
@@ -31,27 +32,8 @@ def on_startup():
     create_db_and_tables()
 
 
-def creation(lst, cls, session, db_film):
-    for entity in lst:
-        db_entity = session.exec(select(cls).where(cls.name == entity.name)).first()
-        if db_entity:
-            db_entity.films.append(db_film)
-        else:
-            db_entity = cls.model_validate(entity)
-            if isinstance(db_entity, Producer):
-                db_film.producers.append(db_entity)
-            elif isinstance(db_entity, Actor):
-                db_film.actors.append(db_entity)
-
-
-def if_not_handler(obj, status_code, detail):
-    if not obj:
-        raise HTTPException(status_code=status_code, detail=detail)
-    return obj
-
-
 @app.post('/films/', response_model=FilmPublicFull)
-def post_film(
+def create_film(
         *,
         session: Session = Depends(get_session),
         film: FilmCreate,
@@ -59,25 +41,31 @@ def post_film(
         actors: list[ActorCreate]
 ):
     db_film = Film.model_validate(film)
-    # for producer in producers:
-    #     db_producer = session.exec(select(Producer).where(Producer.name == producer.name)).first()
-    #     if db_producer:
-    #         db_producer.films.append(db_film)
-    #     else:
-    #         db_producer = Producer.model_validate(producer)
-    #         db_film.producers.append(db_producer)
-    creation(lst=producers, cls=Producer, session=session, db_film=db_film)
-    creation(lst=actors, cls=Actor, session=session, db_film=db_film)
-    session.add(db_film)
-    session.commit()
-    session.refresh(db_film)
-    return db_film
+    creation_routine_handler(
+        lst=producers,
+        cls=Producer,
+        session=session,
+        db_obj=db_film
+    )
+    creation_routine_handler(
+        lst=actors,
+        cls=Actor,
+        session=session,
+        db_obj=db_film
+    )
+    session_routine_handler(obj=db_film, session=session)
+
+
+@app.post('/actors/', response_model=ActorPublic)
+def create_actor(actor: ActorCreate, session: Session = Depends(get_session)):
+    db_actor = Actor.model_validate(actor)
+    return session_routine_handler(obj=db_actor, session=session)
 
 
 @app.get('/films/{film_id}', response_model=FilmPublicFull)
 def get_film(film_id, session: Session = Depends(get_session)):
     db_film = session.get(Film, film_id)
-    return if_not_handler(obj=db_film, status_code=404, detail='Film not Found')
+    return if_not_routine_handler(obj=db_film, status_code=404, detail='Film not Found')
 
 
 @app.get('/films/', response_model=list[FilmPublic])
@@ -88,13 +76,13 @@ def get_films(
         limit: int = Query(default=10, le=10)
 ):
     db_films = session.exec(select(Film).offset(offset).limit(limit)).all()
-    return if_not_handler(obj=db_films, status_code=404, detail='Films not Found')
+    return if_not_routine_handler(obj=db_films, status_code=404, detail='Films not Found')
 
 
 @app.get('/producers/{producer_id}', response_model=ProducerPublicWithFilms)
 def get_producer(*, session: Session = Depends(get_session), producer_id):
     db_producer = session.get(Producer, producer_id)
-    return if_not_handler(obj=db_producer, status_code=404, detail='Producer not Found')
+    return if_not_routine_handler(obj=db_producer, status_code=404, detail='Producer not Found')
 
 
 @app.get('/producers/', response_model=list[ProducerPublic])
@@ -104,13 +92,13 @@ def get_producers(
         limit: int = Query(default=10, le=10)
 ):
     db_producers = session.exec(select(Producer).offset(offset).limit(limit)).all()
-    return if_not_handler(obj=db_producers, status_code=404, detail='Producers not Found')
+    return if_not_routine_handler(obj=db_producers, status_code=404, detail='Producers not Found')
 
 
 @app.get('/actors/{actor_id}', response_model=ActorPublicWithFilms)
 def get_actor(*, session: Session = Depends(get_session), actor_id):
     db_actor = session.get(Actor, actor_id)
-    return if_not_handler(obj=db_actor, status_code=404, detail='Actor not Found')
+    return if_not_routine_handler(obj=db_actor, status_code=404, detail='Actor not Found')
 
 
 @app.get('/actors/', response_model=list[ActorPublic])
@@ -120,7 +108,7 @@ def get_actors(
         limit: int = Query(default=10, le=10)
 ):
     db_actors = session.exec(select(Actor).offset(offset).limit(limit)).all()
-    return if_not_handler(obj=db_actors, status_code=404, detail='Actors not Found')
+    return if_not_routine_handler(obj=db_actors, status_code=404, detail='Actors not Found')
 
 
 if __name__ == '__main__':
